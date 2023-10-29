3b39ed46d242b88d6a9dbb9e566ceef8e1bc3acf
Evgeniy Sharychenkov <sanctio@yandex.ru>
2023-10-29 12:28:59 +0300

    Mod MySecurityConfig

--- src/main/java/ru/sanctio/configuration/MySecurityConfig.java
+++ src/main/java/ru/sanctio/configuration/MySecurityConfig.java
@@ -7,11 +7,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -31,37 +26,13 @@ public class MySecurityConfig {
         return new JdbcUserDetailsManager(dataSource);
     }
 
-    //Хранение данных на уровне приложения
-//    @Bean
-//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsService(BCryptPasswordEncoder passwordEncoder) {
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        manager.createUser(User.withUsername("SHEM")
-//                .password(passwordEncoder.encode("SHEM"))
-//                .roles("EMPLOYEE")
-//                .build());
-//        manager.createUser(User.withUsername("Yana")
-//                .password(passwordEncoder.encode("123qwe"))
-//                .roles("HR")
-//                .build());
-//        manager.createUser(User.withUsername("Mark")
-//                .password(passwordEncoder.encode("123qwe"))
-//                .roles("MANAGER", "HR")
-//                .build());
-//        return manager;
-//    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((user) -> user
-                .requestMatchers(new AntPathRequestMatcher("/api/clients")).hasAnyRole("HR", "MANAGER", "EMPLOYEE")
-                .requestMatchers(new AntPathRequestMatcher("/api/clients/action/**")).hasRole("MANAGER")
+                        .requestMatchers(new AntPathRequestMatcher("/api/clients")).hasAnyRole("HR", "MANAGER", "EMPLOYEE")
+                        .requestMatchers(new AntPathRequestMatcher("/api/clients/action/**")).hasRole("MANAGER")
 //                .requestMatchers(new AntPathRequestMatcher("/hr_info/**")).hasRole("HR")
-                .anyRequest().authenticated()
+                        .anyRequest().authenticated()
         ).formLogin(Customizer.withDefaults());
 
 
